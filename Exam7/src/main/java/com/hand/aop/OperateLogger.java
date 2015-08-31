fd156e9df56cc68ba4953bbea646e2c339bd8327
himegit <524256239@qq.com>
2015-08-31 18:03:43 +0800

    EXAM7

---
+++ Exam7/src/main/java/com/hand/aop/OperateLogger.java
@@ -0,0 +1,15 @@
+package com.hand.aop;
+
+
+
+
+public class OperateLogger {
+	
+	public void before(){
+		System.out.println("Before Save");
+	}
+	
+//	public void after(){
+//		System.out.println("After Insert Film Data");
+//	}
+}
